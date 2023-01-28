e4ea43e434106910b1a483130c9616678f9188df
Tappobill <christian.salvati@hotmail.it>
2023-01-28 19:19:22 +0100

    Completo

---
+++ resources/js/components/Titolo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Titolo da componente</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Titolo'
+    }
+</script>
---
+++ resources/js/views/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Titolo />
+    </div>
+</template>
+
+<script>
+import Titolo from "../components/Titolo.vue"
+
+export default {
+    name: 'App',
+    components: {
+        Titolo
+    }
+}
+</script>
